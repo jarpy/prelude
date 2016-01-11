211f8516e4e35170946390bd1103840b1415ce79
Toby McLaughlin <toby@jarpy.net>
2016-01-12 10:01:19 +1100

    Do not gofmt on save.

--- modules/prelude-go.el
+++ modules/prelude-go.el
@@ -61,7 +61,7 @@
            (setq gofmt-command goimports)))
 
        ;; gofmt on save
-       (add-hook 'before-save-hook 'gofmt-before-save nil t)
+       ;; (add-hook 'before-save-hook 'gofmt-before-save nil t)
 
        ;; stop whitespace being highlighted
        (whitespace-toggle-options '(tabs))
